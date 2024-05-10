4bcb1506ab9a03ef734431f63b82e8b753c03f5c
Raul-Prado <rauloliveiraprado@gmail.com>
2024-05-10 11:06:14 -0300

    Home, Navbar e Footer

---
+++ src/componentes/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Navbar(){
+  return (
+    <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+      <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Farmacia</Link>
+
+            <div className='flex gap-4'>
+            <Link to='/Produtos' className='hover:underline'>Produtos </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
